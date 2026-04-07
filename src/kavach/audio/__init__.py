88d159ace8a2f0f66e746db9c51371f511439af0
anishaman6206 <anishaman6206@gmail.com>
2026-04-07 11:40:04 +0530

    feat(audio): add __init__.py to define public API exports

---
+++ src/kavach/audio/__init__.py
@@ -0,0 +1,10 @@
+from kavach.audio.buffer import ConversationBuffer, Utterance, Speaker
+from kavach.audio.vad    import VoiceActivityDetector, SpeechSegment
+
+__all__ = [
+    "ConversationBuffer",
+    "Utterance",
+    "Speaker",
+    "VoiceActivityDetector",
+    "SpeechSegment",
+]
